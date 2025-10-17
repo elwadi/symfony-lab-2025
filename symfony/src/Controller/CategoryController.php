a0708a4083efbdf50e9c41207ed9b97dfbcc1959
amine <elwadiamine@gmail.com>
2025-10-17 17:35:48 +0100

    symfony form + orm  exemple

--- symfony/src/Controller/CategoryController.php
+++ symfony/src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,8 +23,21 @@ final class CategoryController extends AbstractController
 
 
     #[Route('/category/create', name: 'app_category_create')]
-    public function create(): Response
+    public function create(Request $request, ManagerRegistry $managerRegistry): Response
     {
+
+        if($request->isMethod('POST')) {
+            $category = new Category();
+            $form = $this->createForm(CategoryType::class, $category);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $managerRegistry->getManager()->persist($category);
+                $managerRegistry->getManager()->flush();
+                dd($category);
+
+            } 
+        }
         $data=[];
         $data['form']=$this->createForm(CategoryType::class)->createView();
 
--- symfony/src/Form/CategoryType.php
+++ symfony/src/Form/CategoryType.php
@@ -16,6 +16,7 @@ class CategoryType extends AbstractType
         $builder
             ->add('name',TextType::class,[
                 'label' => 'Nom de la catégorie',
+                'required' => true,
             ])
             ->add('description')
             ->add('icon')
